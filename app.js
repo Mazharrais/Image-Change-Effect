e4ca51b968e58a069c23942039c6843271aab844
Mazharrais <mazharmajo72@gmail.com>
2024-08-07 17:15:13 +0500

    add code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var imgBox = document.querySelector(".img-box");
 var imgWrap = document.querySelector(".img-wrap");
 var originalImg = document.getElementById("originalImg");
+var line = document.getElementById("line");
 
 originalImg.style.width = imgBox.offsetWidth + "px"
 var leftSpace = imgBox.offsetLeft;
@@ -10,4 +11,5 @@ var leftSpace = imgBox.offsetLeft;
 imgBox.onmousemove = function (e){
    var boxWidth = (e.pageX - leftSpace)   + "px";
    imgWrap.style.width = boxWidth;
+   line.style.left = boxWidth;
 }
